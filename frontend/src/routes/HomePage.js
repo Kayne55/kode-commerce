b4e246ce95828f664d3a4dcc111e1f3ffb6d001b
Kayne55 <92627903+KodeSoftwareDev@users.noreply.github.com>
2022-10-30 21:31:01 +0200

    Created-products-api-with-axios

--- frontend/src/routes/HomePage.js
+++ frontend/src/routes/HomePage.js
@@ -1,12 +1,22 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+//import data from '../data';
 
 function HomePage() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="kode-products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="kode-product" key={product.slug}>
             <div className="kode-product-img">
               <Link to={`/product/${product.slug}`}>
@@ -17,7 +27,7 @@ function HomePage() {
               <Link to={`/product/${product.slug}`}>
                 <h3>{product.name}</h3>
               </Link>
-              <p>{product.price}</p>
+              <p className="kode-product-price">R{product.price}</p>
               <button>Add to Cart</button>
             </div>
           </div>
