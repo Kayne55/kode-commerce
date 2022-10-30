de052cd83f963c81bbff53bf50afb4ed9dba06de
Kayne55 <92627903+KodeSoftwareDev@users.noreply.github.com>
2022-10-30 19:04:21 +0200

    Created-Product-Views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Kode Store</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="kode-products">
+          {data.products.map((product) => (
+            <div className="kode-product" key={product.slug}>
+              <div className="kode-product-img">
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+              </div>
+              <div className="kode-product-info">
+                <a href={`/product/${product.slug}`}>
+                  <h3>{product.name}</h3>
+                </a>
+                <p>{product.price}</p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Polo Mens slim fit gingham shirt',
+      slug: 'polo-mens-slim-shirt',
+      category: 'Shirts',
+      image: '/images/polo-mens-slim-shirt.jpg',
+      price: 1150,
+      countInStock: 15,
+      brand: 'Polo',
+      rating: 4.5,
+      numReviews: 8,
+      description: 'Polo Mens slim fit gingham cotton shirt.',
+    },
+    {
+      name: 'Levi 511 Slim Fit Jeans',
+      slug: 'levi-511-jeans',
+      category: 'Pants',
+      image: '/images/levi-511-jeans.jpg',
+      price: 899,
+      countInStock: 25,
+      brand: 'Levi',
+      rating: 5,
+      numReviews: 12,
+      description:
+        'A modern slim with room to move; the 511 slim fit stretch jeans are a classic since right now. These jeans sit below the waist with a slim fit from hip to ankle. Cut close to the body, the 511 Slim is a great alternative to the skinny jeans.',
+    },
+    {
+      name: 'Hurley Phantom Walkshorts',
+      slug: 'hurley-phantom-walkshorts',
+      category: 'Shorts',
+      image: '/images/hurley-phantom-walkshorts.jpg',
+      price: 499,
+      countInStock: 15,
+      brand: 'Hurley',
+      rating: 4,
+      numReviews: 5,
+      description:
+        'The phantom 20" walkshort offers 4-way stretch and quick-dry capabilities, meaning it will keep you performing to the max, whatever your day brings.',
+    },
+    {
+      name: 'Adidas Grand Court SE Shoes',
+      slug: 'adidas-grand-court-se-shoes',
+      category: 'Shoes',
+      image: '/images/adidas-grand-court-se-shoes.jpg',
+      price: 1599,
+      countInStock: 9,
+      brand: 'Adidas',
+      rating: 5,
+      numReviews: 8,
+      description:
+        "Vintage vibes. New soul. These shoes have a clean and contemporary design with the bones of '70s-era tennis trainers. This version has soft suede overlays and and a textured leather upper.",
+    },
+  ],
+};
+
+export default data;
