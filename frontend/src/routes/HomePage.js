1e89bb672983990d1d1f511a3305ed4be768e219
Kayne55 <kayne.jeacocks@gmail.com>
2022-11-01 22:25:53 +0200

    Created-Product-and-Rating-Components

--- frontend/src/routes/HomePage.js
+++ frontend/src/routes/HomePage.js
@@ -1,7 +1,9 @@
 import { useEffect, useReducer } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import logger from 'use-reducer-logger';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
 //import data from '../data';
 
 const reducer = (state, action) => {
@@ -47,22 +49,13 @@ function HomePage() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="kode-product" key={product.slug}>
-              <div className="kode-product-img">
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-              </div>
-              <div className="kode-product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p className="kode-product-price">R{product.price}</p>
-                <button className="btn btn-primary">Add to Cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col sm={6} md={4} lg={3}>
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
