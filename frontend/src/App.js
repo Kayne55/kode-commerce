35138253490c6e208863a9fe735c3d6fe27afb41
Kayne55 <kayne.jeacocks@gmail.com>
2022-11-13 18:45:03 +0200

    Created-Sign-In-Page-UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartPage from './routes/CartPage';
+import SigninPage from './routes/SigninPage';
 
 function App() {
   const { state } = useContext(Store);
@@ -41,6 +42,7 @@ function App() {
             <Routes>
               <Route path="/product/:slug" element={<ProductPage />} />
               <Route path="/cart" element={<CartPage />} />
+              <Route path="/signin" element={<SigninPage />} />
               <Route path="/" element={<HomePage />} />
             </Routes>
           </Container>
