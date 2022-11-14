e66a20d18f66a1bb7d53447ca3111c8ba02bc571
Kayne55 <kayne.jeacocks@gmail.com>
2022-11-14 21:14:58 +0200

    Created-User-Model-and-Routes

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Admin',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('admin1234'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'john@example.com',
+      password: bcrypt.hashSync('john1234'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
